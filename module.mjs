ef0fc770db92ebe7032cc69ffca8c25f953d1422
Jesus Vargas <jesusvarguer18@gmail.com>
2022-04-15 14:19:24 -0500

    subiendo archivos de los 4 pilares de la OOP en JS

---
+++ module.mjs
@@ -0,0 +1,15 @@
+import { PlatziClass } from "./main.mjs";
+
+const clase67 = new PlatziClass({
+  name: "JavaCript: orientados a objeto, basado en prototipos",
+  videoID: "sdasdasd; dfarhrtsfbfg",
+});
+
+clase67.reprocudir();
+clase67.pausar();
+
+clase67.reprocudir();
+clase67.pausar();
+
+clase67.reprocudir();
+clase67.pausar();
